e1c98fb15c7470b08129278f8903f40f2f54638c
Nina <alhimovich.nina@gmail.com>
2026-03-18 20:56:23 +0300

    Implement F1 service methods

--- service/f1_service.py
+++ service/f1_service.py
@@ -1,9 +1,34 @@
 import fastf1
-from datetime import date, timedelta
-from zoneinfo import ZoneInfo
+import pandas as pd
+from datetime import datetime, timedelta
+import zoneinfo
 import logging
+from config.config import tz_map, DATA_DIR
 
 logger = logging.getLogger(__name__)
+fastf1.Cache.enable_cache(str(DATA_DIR / 'fastf1_cache'))
+
+days_to_wed = 4
+
+def get_next_event():
+    year = datetime.now().year
+
+    try:
+        schedule = fastf1.get_event_schedule(year)
+    except Exception as e:
+        logger.error(f"Failed to load F1 calendar: {e}")
+        return []
+
+    next_event = schedule[pd.to_datetime(schedule["EventDate"]) > pd.Timestamp.now(tz="UTC").to_datetime64()]
+    return next_event.iloc[0] if not next_event.empty else next_event
+
+def get_fia_slug(event_name: str) -> str:
+    return event_name.lower().replace(" grand prix", "-gp").replace(" ", "-")
+
+def get_local_pre_media_day(event) -> datetime:
+    race_local = event['Session5Date']
+    wednesday = race_local - timedelta(days=days_to_wed)
+    return wednesday.replace(hour=0, minute=0, second=0, microsecond=0)
 
 def get_f1_calendar(year: int = 2026):
     """
@@ -19,7 +44,6 @@ def get_f1_calendar(year: int = 2026):
     }
     """
     try:
-        fastf1.Cache.enable_cache('../fastf1_cache')
         schedule = fastf1.get_event_schedule(year)
     except Exception as e:
         logger.error(f"Failed to load F1 calendar: {e}")
@@ -36,36 +60,6 @@ def get_f1_calendar(year: int = 2026):
         friday_offset = 2
 
         first_day = race_date - timedelta(days=friday_offset)
-
-        tz_map = {
-            'Melbourne': 'Australia/Melbourne',
-            'Shanghai': 'Asia/Shanghai',
-            'Suzuka': 'Asia/Tokyo',
-            'Sakhir': 'Asia/Bahrain',
-            'Jeddah': 'Asia/Riyadh',
-            'Miami Gardens': 'America/New_York',
-            'Montréal': 'America/Toronto',
-            'Monte Carlo': 'Europe/Monaco',
-            'Barcelona': 'Europe/Madrid',
-            'Spielberg': 'Europe/Vienna',
-            'Silverstone': 'Europe/London',
-            'Spa-Francorchamps': 'Europe/Brussels',
-            'Budapest': 'Europe/Budapest',
-            'Zandvoort': 'Europe/Amsterdam',
-            'Monza': 'Europe/Rome',
-            'Madrid': 'Europe/Madrid',
-            'Baku': 'Asia/Baku',
-            'Marina Bay': 'Asia/Singapore',
-            'Austin': 'America/Chicago',
-            'Mexico City': 'America/Mexico_City',
-            'São Paulo': 'America/Sao_Paulo',
-            'Las Vegas': 'America/Los_Angeles',
-            'Lusail': 'Asia/Qatar',
-            'Yas Marina': 'Asia/Dubai',
-
-            # Fallback
-            '': 'UTC'
-        }
 
         tz_name = tz_map.get(row['Location'], 'UTC')
         if tz_name == 'UTC':
